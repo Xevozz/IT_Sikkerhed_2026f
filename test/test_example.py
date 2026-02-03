771b3dfd321e670f8584680c83379936a9236994
Xevoz_ <Niklas.loev@gmail.com>
2026-02-03 12:23:08 +0100

    Rettelser ang. unittest der fejler / crasher viser Passed, hvis de netop fejler / crasher

--- test/test_example.py
+++ test/test_example.py
@@ -15,39 +15,41 @@ def test_pass_with_calculation():
 
 
 # Test der fejler #
-
 def test_fail():
-    assert 1 + 1 == 3 # forkert, skal være 2
+    result = 1 + 1
+    assert result != 3 # forkert, 1 + 1 er 2 ikke 3
 
 def test_another_fail():
-    assert "hello".lower() == "HELLO" # forkert, skal være "hello"
+    assert "hello".lower() != "HELLO" # forkert, skal være "hello"
 
+@pytest.mark.focus(reason="This test is expected to fail.") # forventet fejl
 def test_fail_with_exception():
-    raise ValueError("This is an intentional failure.") # forventet fejl
+    with pytest.raises(ValueError):
+        raise ValueError("This is an intentional failure.") # forventet fejl
 
 
 
 # Test som crasher #
-
 def test_crash():
-    raise Exception("This test crashes!") # En generel undtagelse
+    with pytest.raises (Exception):
+        raise Exception("This test crashes!") # En generel undtagelse
 
 def test_another_crash():
-    x = 1 / 0  # man kan ikke dividere med 0
+    with pytest.raises(ZeroDivisionError):
+        _ = 1 / 0 # man kan ikke dividere 0
 
 def test_crash_with_index():
     lst = [1, 2, 3]
-    _ = lst[5]  # 5 findes ikke i index-listen angivet.
+    with pytest.raises(IndexError):
+        _ = lst[5] # 5 findes ikke i index-listen angivet.
 
 
 
 # Test der skippes #
 @pytest.mark.skip(reason="Skipping this test for demonstration purposes.")
-
 def test_skip():
     assert 1 + 1 == 2 # testen bliver skippet
 
-@pytest.mark.xfail(reason="This test is expected to fail.") # testen bliver skippet
-
+@pytest.mark.skip(reason="This test is expected to fail.") # testen bliver skippet
 def test_expected_fail():
     assert 1 + 1 == 3 # testen bliver skippet
